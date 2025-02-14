2e07fc6996e24da694419850a361d6415686dce8
Kerdon <111719813+Candog85@users.noreply.github.com>
2025-02-14 09:02:20 -0500

    Done! (#4)

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 import pymysql
 from dynaconf import Dynaconf
 import flask_login
+import requests
+import csv
+import json
 
 # Declare Flask application
 app = Flask(__name__)
@@ -89,3 +92,34 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+def requestinfo(schoolname='', schoolstate=''):
+
+    count=0
+
+    api="https://api.data.gov/ed/collegescorecard/v1/schools?api_key=fEZsVdtKgtVU4ODIpjHcP8vDttDK0ftSGZaWDcAk"
+
+    queries={}
+    request=''
+
+    if schoolname:
+        queries.update({"school.name":(f"{schoolname}")})
+    
+    if schoolstate:
+        queries.update({"school.state":(f"{schoolstate}")})
+
+    for query in queries:
+        request+=(f"{query}={queries[query]}")
+        count+=1
+    
+    request=f"{api}&{request}"
+
+    test=requests.get(request).json()
+
+    for key in test:{ 
+        print(key,":", test[key]) 
+    }
+        
+    return request
+
+print(requestinfo("Harvard University"))
