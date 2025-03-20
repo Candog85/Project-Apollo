b57d172aeac7a2f7527cf86c3d2705be6445b864
Kerdon <111719813+Candog85@users.noreply.github.com>
2025-03-20 10:07:25 -0400

    10045 90 fix access to input page for users without college parameters (#41)
    * Login+Signin functionality
    
    * Also did alerts and stuff
    
    * Cool beans ahaa
    
    ---------
    
    Co-authored-by: Farhan Chowdury <fchowdury@brooklynsteamcenter.org>

--- main.py
+++ main.py
@@ -193,18 +193,31 @@ def user_input():
     SELECT * 
     FROM `User`
     WHERE `id` = %s               
-                   
+
     """, (customer_id))
     
     results=cursor.fetchall()
     
     results=results[0]
     
+
+    print(results)
+    
+    if results['sat_score']==None:
+        results['sat_score']=0
+    
+    if results['tuition_budget']==None:
+        results['tuition_budget']=0
+    
+    if results['population_preferences']==None:
+        results['population_preferences']=0
+    
     results['tuition_budget']=(f"${results['tuition_budget']:,d}")
     
     results['population_preferences']=(f"{results['population_preferences']:,d}")
     
     print(results)
+
     return render_template("settings.html.jinja", results=results)
     
 
