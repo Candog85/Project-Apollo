95c2d34f8f4537fff0c74fec503009778abb14e9
Kerdon <111719813+Candog85@users.noreply.github.com>
2025-04-03 08:57:53 -0400

    Lots o stuff (#52)
    -Adding and removing colleges
    -graph display
    -Dynamic college pages based on if added

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # All imports
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, Response
 import pymysql
 from dynaconf import Dynaconf
 import flask_login
 import requests
+import io
+import random
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from math import radians, cos, sin, asin, sqrt
+import matplotlib.pyplot as plt
+import base64
 
 # Declare Flask application
 app = Flask(__name__)
@@ -186,7 +193,8 @@ def college_browse():
 # Analytics page (college and user graphs for comparison and analysis)
 @app.route("/analytics", methods=["Post", "GET"])
 def analytics_page():
-    
+
+    #Browse area
     page = int(request.args.get('page', '1'))
     query = request.args.get('query')
     
@@ -217,12 +225,181 @@ def analytics_page():
     cursor.close()
     conn.close()
     
-    return render_template("analytics.html.jinja", colleges=colleges, page=page, query=query)
-    # Note: For now, the database connection and data fetcher are placeholders. This WILL be changed later as neccessary.
+    return render_template("analytics.html.jinja", colleges=colleges, page=page, query=query, customer_id=customer_id)
+    # Note: For now, the database connection and data fetcher are placeholders. This WILL be changed later as neccessary.  
+
+@app.route('/plot')
+def create_figure():
+    #Constant id (change to dynamic later)
+    customer_id=flask_login.current_user.id
+
+    #Settings for which graph to generate (will change dynamically)
+    comparing_catagory=2
+
+
+    compare_sat=False
+    compare_tuition=False
+    compare_distance=False
+
+    if comparing_catagory==0:
+        compare_sat=True
+        
+    elif comparing_catagory==1:
+        compare_tuition=True
+        
+    elif comparing_catagory==2:
+        compare_distance=True
+
+    if compare_sat==True:
+        comparing="Student SAT Score vs College SAT Admission Requirements"
+        
+    elif compare_tuition==True:
+        comparing="Student Tuition Budget vs Average College Tuition"
+
+    elif compare_distance==True:
+        comparing="Distance between Colleges and User's Zip Code"
+        
+
+
+    #Database connection
+    conn = connect_db()
+    cursor=conn.cursor()
+
+    #Selects college info from every college on the user's list
+    cursor.execute(f"""
+
+    SELECT `name`, `tuition`, `admission_rate`, `average_sat`, `size`, `city`, `state`, `longitude`, `latitude`
+    FROM `CollegeList`
+    LEFT JOIN `Colleges`
+    ON `CollegeList`.`college_id` = `Colleges`.`id`
+    AND `CollegeList`.`user_id`= %s 
+           """,(customer_id))
+
+    #Stores college results
+    college_results=cursor.fetchall()
+
+    #Selects user's info based on their id
+    cursor.execute(f"""
+                    
+    SELECT `first_name`, `sat_score`, `tuition_budget`, `zip_code`
+    FROM `User`
+    WHERE `id` = %s  
+                    
+                    """,(customer_id))
+
+    #Stores user info, then converts into a single dictionary
+    student_results=cursor.fetchall()
+    student_results=student_results[0]
+
+    if compare_distance==True:
+        names=[]
+        
+    else:
+        #Initializes name list with first index being the user's name
+        names=[student_results['first_name']]
+
+
+
+    #If each setting is selected, data list is set to the selected data comparisor
+    if compare_sat==True:
+        data=[student_results['sat_score']]
+
+    elif compare_tuition==True:
+        data=[student_results['tuition_budget']]
+
+    if compare_distance==True:
+        data=[]
+
+    print(college_results)
+
+    for college in college_results:
+        names.append(college["name"])
+
+        if compare_sat==True:
+            if college["average_sat"]==None:
+                data.append(0)
+            else:
+                data.append(college["average_sat"])
+
+        elif compare_tuition==True:
+            if college["tuition"]==None:
+                data.append(0)
+            else:
+                data.append(college['tuition'])
+                
+        elif compare_distance==True:
+            
+            cursor.execute(f"""
+                        
+            SELECT `lat`, `lng`
+            FROM `Locations`
+            WHERE `zip` = %s               
+                        
+                        """, (student_results['zip_code']))
+            
+            student_coordinates=cursor.fetchone()
+            
+            def haversine(lon1, lat1, lon2, lat2):
+                """
+                Calculate the great circle distance in kilometers between two points 
+                on the earth (specified in decimal degrees)
+                """
+                # convert decimal degrees to radians 
+                lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+                # haversine formula 
+                dlon = lon2 - lon1 
+                dlat = lat2 - lat1 
+                a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+                c = 2 * asin(sqrt(a)) 
+                r = 3956 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+                return c * r
+            
+            print(college_results)
+            print(student_coordinates)
+            
+            print(college['longitude'])
+            print(college['latitude'])
+            
+            data.append(int(haversine(student_coordinates['lng'], student_coordinates['lat'], college['longitude'], college['latitude'])))
+
+
+    fig=plt.figure(figsize=(10, 6), facecolor='#202020', edgecolor='#DEB64B')
+
+    bar=plt.bar(names,data, color='#DEB64B', edgecolor='#202020', )
+    plt.title(f"{comparing}")
+
+    bar[0].set_color('#202020')
+    bar[0].set_edgecolor('#DEB64B')
+
+    ax=plt.gca()
+
+    ax.set_facecolor('#202020')
+
+    ax.spines['top'].set_color('#DEB64B')
+    ax.spines['right'].set_color('#DEB64B')
+    ax.spines['bottom'].set_color('#DEB64B')
+    ax.spines['left'].set_color('#DEB64B')
+
+    ax.tick_params(axis='x', colors='#DEB64B')
+
+    ax.tick_params(axis='y', colors='#DEB64B')
+
+    plt.xticks(rotation=320, ha='left')
+
+    if compare_tuition==True:
+        ax.yaxis.set_major_formatter('${x:,.0f}')
+        
+    plt.tight_layout()
+        
+    plt.savefig(f'static/images/plots/{customer_id}.png',dpi=100)
     
+    return redirect('/analytics')
+
 @app.route("/college/<college_id>", methods=["POST", "GET"])
 def college(college_id):
     
+    customer_id=flask_login.current_user.id
     
     conn = connect_db()
     cursor = conn.cursor()
@@ -248,8 +425,20 @@ def college(college_id):
         
     college['size']=f"{college['size']:,}"
     
+    cursor.execute(f"""
+                   
+    SELECT * from `CollegeList`
+    WHERE `user_id`={customer_id} AND `college_id`={college_id}            
+                   """)
     
-    return render_template("college.html.jinja", college_id=college_id, college=college)
+    added=cursor.fetchall()
+    
+    if added==():
+        added=False
+    else:
+        added=True    
+    
+    return render_template("college.html.jinja", college_id=college_id, college=college, added=added)
 
 @app.route("/college/<college_id>/add", methods=["POST", "GET"])
 def add_college(college_id):
@@ -261,10 +450,29 @@ def add_college(college_id):
     
     cursor.execute(f"""
                    
-    INSERT INTO `CollegeList` (`list_id`, `college_id`)
+    INSERT INTO `CollegeList` (`user_id`, `college_id`)
     VALUES (%s, %s)
                    
                    """,(customer_id, college_id))
+    
+    return redirect(f"/college/{college_id}")
+
+@app.route("/college/<college_id>/remove", methods=["POST", "GET"])
+def remove_college(college_id):
+    
+    customer_id=flask_login.current_user.id
+    
+    conn=connect_db()
+    cursor=conn.cursor()
+    
+    cursor.execute(f"""
+                   
+    DELETE FROM `CollegeList`
+    WHERE `user_id`= {customer_id} AND `college_id`= {college_id}
+                   
+                   """)
+    
+    return redirect(f"/college/{college_id}")
 
 # User input on settings page
 @app.route("/settings", methods=["POST", "GET"])
