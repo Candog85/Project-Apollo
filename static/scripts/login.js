aa26f78b00077e82d5f12320a5de0b027a0291c9
Noel ☆ <158505019+Noel8008@users.noreply.github.com>
2025-05-02 10:05:48 -0400

    finally (#92)

---
+++ static/scripts/login.js
@@ -0,0 +1,21 @@
+/// Show/hide password functions
+// Declare variables and link to HTML elements
+let togglePassword = document.getElementById('togglePassword');
+let toggleRePassword = document.getElementById('toggleRePassword');
+let initialPasswordField = document.getElementById('floatingPassword');
+let confirmPasswordField = document.getElementById('floatingPassword2');
+
+// Toggle initial password visibility 
+togglePassword.addEventListener('click', function () {
+    let type = initialPasswordField.type === 'password' ? 'text' : 'password';
+    initialPasswordField.type = type;
+    togglePassword.textContent = type === 'password' ? 'Show Password' : 'Hide Password';
+});
+
+// Toggle re-enter password visibility
+toggleRePassword.addEventListener('click', function () {
+    let type = confirmPasswordField.type === 'password' ? 'text' : 'password';
+    confirmPasswordField.type = type;
+    toggleRePassword.textContent = type === 'password' ? 'Show Password' : 'Hide Password';
+});
+/// End of show/hide pwd funcs
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,24 +1,3 @@
-/// Show/hide password functions
-// Declare variables and link to HTML elements
-let togglePassword = document.getElementById('togglePassword');
-let toggleRePassword = document.getElementById('toggleRePassword');
-let initialPasswordField = document.getElementById('floatingPassword');
-let confirmPasswordField = document.getElementById('floatingPassword2');
-
-// Toggle initial password visibility 
-togglePassword.addEventListener('click', function () {
-    let type = initialPasswordField.type === 'password' ? 'text' : 'password';
-    initialPasswordField.type = type;
-    togglePassword.textContent = type === 'password' ? 'Show Password' : 'Hide Password';
-});
-
-// Toggle re-enter password visibility
-toggleRePassword.addEventListener('click', function () {
-    let type = confirmPasswordField.type === 'password' ? 'text' : 'password';
-    confirmPasswordField.type = type;
-    toggleRePassword.textContent = type === 'password' ? 'Show Password' : 'Hide Password';
-});
-/// End of show/hide pwd funcs
 
 // Tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
