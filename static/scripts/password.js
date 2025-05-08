f3f44bccaec88d6b134c3bb4a37c138be49cd879
Farhan Chowdury <fchowdury@brooklynsteamcenter.org>
2025-05-08 10:05:52 -0400

    Change show/hide pwd to icons (#99)
    It’s fire

--- static/scripts/password.js
+++ static/scripts/password.js
@@ -1,4 +1,3 @@
-// Declare variables and link to HTML elements
 let togglePassword = document.getElementById('togglePassword');
 let toggleRePassword = document.getElementById('toggleRePassword');
 let initialPasswordField = document.getElementById('floatingPassword');
@@ -6,16 +5,18 @@ let confirmPasswordField = document.getElementById('floatingPassword2');
 
 // Toggle initial password visibility 
 togglePassword.addEventListener('click', function () {
+    let icon = togglePassword.querySelector('i');
     let type = initialPasswordField.type === 'password' ? 'text' : 'password';
     initialPasswordField.type = type;
-    togglePassword.textContent = type === 'password' ? 'Show Password' : 'Hide Password';
+    icon.classList.toggle('bi-eye');
+    icon.classList.toggle('bi-eye-slash');
 });
 
 // Toggle re-enter password visibility
 toggleRePassword.addEventListener('click', function () {
+    let icon = toggleRePassword.querySelector('i');
     let type = confirmPasswordField.type === 'password' ? 'text' : 'password';
     confirmPasswordField.type = type;
-    toggleRePassword.textContent = type === 'password' ? 'Show Password' : 'Hide Password';
+    icon.classList.toggle('bi-eye');
+    icon.classList.toggle('bi-eye-slash');
 });
-
-
